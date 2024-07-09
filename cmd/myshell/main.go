498e04d03073838f101e0dfadb45df8a627fbc2d
SatyajitMohanty57 <mohantysatyajit57@gmail.com>
2024-07-09 10:41:56 -0400

    pass 2nd stage

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -9,21 +9,15 @@ import (
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 
-	//Uncomment this block to pass the first stage
-	fmt.Fprint(os.Stdout, "$ ")
-
-	// Wait for user input
-	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
-
-	if err != nil {
-		fmt.Println("Error encountered while receiving input")
+	for {
+		_, err := fmt.Fprint(os.Stdout, "$ ")
+		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		if err != nil {
+			fmt.Println("Error encountered while receiving input")
+		}
+		_, err = fmt.Fprint(os.Stdout, command[:len(command)-1]+": command not found\n")
+		if err != nil {
+			return
+		}
 	}
-
-	_, err = fmt.Fprint(os.Stdout, command[:len(command)-1]+": command not found\n")
-	if err != nil {
-		return
-	}
-
-	command, err = bufio.NewReader(os.Stdin).ReadString('\n')
-
 }
